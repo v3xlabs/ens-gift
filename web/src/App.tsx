22eae09670a7a2c0cf8a09e59bb9243a8abb3c1f
Jakob Helgesson <jakob.helgesson@gmail.com>
2024-03-17 13:08:15 +0000

    Lint files

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,16 @@
-import { TransactionHistory } from "./txHistory/TransactionHistory";
+import { TransactionHistory } from './txHistory/TransactionHistory';
 
 export const App = () => {
-
     return (
         <div className="w-full h-full min-h-screen bg-light-background-secondary dark:bg-dark-background-secondary">
             <div className="mx-auto w-full max-w-2xl space-y-4 py-8">
                 <div className="card w-full p-4">
                     <h1 className="text-xl">UltraBulk.eth</h1>
                     <p>
-                        A lightweight and gas-optimized smart contract focused on ENS <span className="font-bold">.eth</span> registrations and renewals.
-                        With as much flexibility as you might need.
+                        A lightweight and gas-optimized smart contract focused
+                        on ENS <span className="font-bold">.eth</span>{' '}
+                        registrations and renewals. With as much flexibility as
+                        you might need.
                     </p>
                 </div>
                 <div className="flex gap-4 w-full flex-col md:flex-row">
--- web/src/txHistory/TransactionEntry.tsx
+++ web/src/txHistory/TransactionEntry.tsx
@@ -1,10 +1,9 @@
-import { FC } from "react";
+import { FC } from 'react';
 
-export const TransactionEntry: FC<{txHash: string}> = (txHash) => {
-
+export const TransactionEntry: FC<{ txHash: string }> = ({ txHash }) => {
     return (
         <div className="p-4 card w-full">
             Transaction: <span>{txHash}</span>
         </div>
-    )
+    );
 };
